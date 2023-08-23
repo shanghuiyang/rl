b24b1d65d9211865c1f216c6b710d162edd773f6
Shanghui Yang <shanghui.yang@grabtaxi.com>
2023-08-23 22:48:05 +0800

    better codes

--- contents/2_Q_Learning_maze/q_learing.py
+++ contents/2_Q_Learning_maze/q_learing.py
@@ -1,19 +1,12 @@
-"""
-This part of code is the Q learning brain, which is a brain of the agent.
-All decisions are made in here.
-
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
-
 import numpy as np
 import pandas as pd
 
 Q_TABLE_CSV = 'q-table.csv'
 
 
-class QLearningTable:
+class QLearning:
     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9, load_qtable_from_csv=False, save_qtable_to_csv=False):
-        self.actions = actions  # a list
+        self.actions = actions
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
@@ -31,44 +24,26 @@ class QLearningTable:
 
     def choose_action(self, observation):
         self.check_state_exist(observation)
-        # action selection
         if np.random.uniform() < self.epsilon:
-            # choose best action
             state_action = self.q_table.loc[observation, :]
-            # print(f'state_action type: {type(state_action)}, state_action: \n{state_action}')
-            # some actions may have the same value, randomly choose on in these actions
             action = np.random.choice(state_action[state_action == np.max(state_action)].index)
-            # print(f'action: {action}')
         else:
-            # choose random action
             action = np.random.choice(self.actions)
-        # print(f'action: {action}')
         return action
 
     def learn(self, s, a, r, s_, terminated):
         self.check_state_exist(s_)
         q_predict = self.q_table.loc[s, a]
         if not terminated:
-            q_target = r + self.gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
         else:
-            q_target = r  # next state is terminal
-            # self.q_table.loc[s_, a] = r
+            q_target = r
 
-        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
-        # print(f'state={s}, old_q_value={q_predict:.6f}, new_q_value={self.q_table.loc[s, a]:.6f}')
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)
         return self.q_table
 
     def check_state_exist(self, state):
         if state not in self.q_table.index:
-            # append new state to q table
-            
-            # self.q_table = self.q_table.append(
-            #     pd.Series(
-            #         [0]*len(self.actions),
-            #         index=self.q_table.columns,
-            #         name=state,
-            #     )
-            # )
             new_row = pd.Series(
                     [0]*len(self.actions),
                     index=self.q_table.columns,
@@ -77,6 +52,7 @@ class QLearningTable:
             self.q_table.loc[state] = new_row
 
     def save_qtable(self):
-        if self.save_qtable_to_csv:
-            self.q_table.sort_index(inplace=True)
-            self.q_table.to_csv(Q_TABLE_CSV, index=True)
+        if not self.save_qtable_to_csv:
+            return
+        self.q_table.sort_index(inplace=True)
+        self.q_table.to_csv(Q_TABLE_CSV, index=True)
--- contents/2_Q_Learning_maze/run_this.py
+++ contents/2_Q_Learning_maze/run_this.py
@@ -1,89 +1,64 @@
-"""
-Reinforcement learning maze example.
-
-Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
-
-This script is the main part which controls the update method of this example.
-The RL is in RL_brain.py.
-
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
-
 import pandas as pd
-from RL_brain import QLearningTable
+from q_learing import QLearning
 import gym
 
+MAX_EPISODE = 200
 
-def update():
-    for episode in range(100):
-        # initial state
-        state, info = env.reset()
-        steps = 0
-        # done = False
-        result = 'truncated'
-        while True:
-            steps += 1
-            # fresh env
-            env.render()
+pd.set_option('display.float_format', lambda x: '%.8f' % x)
+env = gym.make(
+    "FrozenLake-v1",
+    is_slippery=False,
+    render_mode="human",
+    map_name='4x4',
+    desc=[
+        "SFFF",
+        "FFHF",
+        "FHGF",
+        "FFFF",
+    ],
+)
+# env = env.unwrapped
+ql = QLearning(
+    actions=list(range(env.action_space.n)),
+    learning_rate=0.1,
+    load_qtable_from_csv=False,
+    save_qtable_to_csv=False,
+)
 
-            # RL choose action based on state
-            action = RL.choose_action(state)
+# training
+for episode in range(MAX_EPISODE):
+    steps = 0
+    result = 'truncated'
+    terminated, truncated = False, False
+    state, info = env.reset()
+    while not terminated and not truncated:
+        steps += 1
+        env.render()
+        action = ql.choose_action(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        print(f'state: {state}, action: {action}, next_state: {next_state}, reward: {reward}')
 
-            # RL take action and get next state and reward
-            next_state, reward, terminated, truncated, info = env.step(action)
-            print(f'state: {state}, action: {action}, next_state: {next_state}, reward: {reward}')
-            
-            # ------------
-            if next_state == 10:
-                reward = 1
-                result = 'win'
-            elif next_state == 6 or next_state == 9:
-                reward = -1
-                result = 'dead'
-            else:
-                reward = 0
-            # -------------
-            # RL learn from this transition
-            qtable = RL.learn(state, action, reward, next_state, terminated)
+        # -- customed reward -------
+        reached_the_gold = (terminated and reward == 1)
+        fell_in_a_hole = (terminated and reward == 0)
+        if reached_the_gold:
+            reward = 1
+            result = 'win'
+        elif fell_in_a_hole:
+            reward = -1
+            result = 'miss'
+        else:
+            reward = 0
+        # --------------------------
 
-            state = next_state
+        qtable = ql.learn(state, action, reward, next_state, terminated)
+        state = next_state
 
-            # break while loop when end of this episode
-            if terminated or truncated:
-                print(f'---- episode = {episode}, steps = {steps}, result = {result} ----')
-                print(qtable.sort_index())
-                print('----------------------------------------------\n')
-                break
-
-    # end of game
-    RL.save_qtable()
-    print('game over')
+    print(f'------- episode: {episode+1}, steps: {steps}, result: {result} --------')
+    print(qtable.sort_index())
+    print('--------------------------------------------------\n')
 
 
-if __name__ == "__main__":
-    pd.set_option('display.float_format', lambda x: '%.6f' % x)
-    # --- gym version ---
-    env = gym.make(
-        "FrozenLake-v1",
-        is_slippery=False,
-        render_mode="human",
-        map_name='4x4',
-        desc=[
-            "SFFF",
-            "FFHF",
-            "FHGF",
-            "FFFF",
-        ],
-    )
-    # env = env.unwrapped
-    RL = QLearningTable(
-        actions=list(range(env.action_space.n)),
-        learning_rate=0.1,
-        load_qtable_from_csv=False,
-        save_qtable_to_csv=False,
-    )
-    update()
-    env.close()
+ql.save_qtable()
+env.close()
+print('training done')
